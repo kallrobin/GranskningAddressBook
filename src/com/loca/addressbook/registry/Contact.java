a5215558fab8cbe04f8ba86f41d1a88f456e8fb1
OlaBKarlsson <ola.karlsson@nackademin.se>
2016-12-20 12:16:02 +0100

    Added Contact java file

---
+++ src/com/loca/addressbook/registry/Contact.java
@@ -0,0 +1,12 @@
+package com.loca.addressbook.registry;
+
+/**
+ * Created by katan on 2016-12-20.
+ */
+public interface Contact {
+
+    String  getId();
+    void getFirstName();
+    void getLastName();
+    void getEmailAddress();
+}
