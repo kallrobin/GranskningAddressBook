0362319746481b66808b15f112f73b5019e37ad9
Linus Blom <linus.blom@nackademin.se>
2016-12-20 15:59:41 +0100

    Added helpmenu

--- src/com/loca/addressbook/userinterface/commands/HelpCommand.java
+++ src/com/loca/addressbook/userinterface/commands/HelpCommand.java
@@ -1,15 +1,20 @@
 package com.loca.addressbook.userinterface.commands;
 
 import com.loca.addressbook.exceptions.InvalidCommandParameterException;
+import com.loca.addressbook.userinterface.ConsolePrinter;
 
 import java.util.List;
 
 public class HelpCommand implements Command {
+    private static final String NEW_LINE = "\n";
+    private static final String TAB = "\t";
     private CommandType commandType = CommandType.HELP;
     private List<String> parameters;
+    private ConsolePrinter consolePrinter;
 
-    public HelpCommand (List<String> parameters) {
+    public HelpCommand (ConsolePrinter consolePrinter, List<String> parameters) {
         this.parameters = parameters;
+        this.consolePrinter = consolePrinter;
     }
 
     @Override
@@ -28,6 +33,25 @@ public class HelpCommand implements Command {
             throw new InvalidCommandParameterException();
         }
 
+        String helpText = makeHelpText();
+        showHelpText(helpText);
+
+    }
+
+    private String makeHelpText() {
+        StringBuilder helpText = new StringBuilder(100);
+        for (CommandType commandType: CommandType.values()) {
+            helpText.append(commandType.getCommandName());
+            helpText.append(TAB);
+            helpText.append(commandType.getDescription());
+            helpText.append(NEW_LINE);
+        }
+        return helpText.toString();
+    }
+
+
+    private void showHelpText(String helpText) {
+        consolePrinter.print(helpText);
     }
 
 }
