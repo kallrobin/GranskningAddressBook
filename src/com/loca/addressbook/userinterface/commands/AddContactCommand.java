9bfc008f2d50ef4e5c75b676e0c7530ac9cf8124
christian <christian@ubuntu>
2016-12-20 14:46:54 +0100

    CommandType m.m.

--- src/com/loca/addressbook/userinterface/commands/AddContactCommand.java
+++ src/com/loca/addressbook/userinterface/commands/AddContactCommand.java
@@ -1,8 +1,22 @@
 package com.loca.addressbook.userinterface.commands;
 
+import java.util.List;
+
+import com.loca.addressbook.registry.Registry;
 import com.loca.addressbook.userinterface.exceptions.InvalidCommandParameterException;
 
 public class AddContactCommand implements Command {
+	
+	private static final int REQUIRED_PARAMETERS_COUNT = 3;
+	CommandType commandType = CommandType.ADD;
+	Registry registry;
+	List<String> parameters;
+	
+	public AddContactCommand (Registry registry, List<String> parameters) {
+		this.registry = registry;
+		this.parameters = parameters;
+	}
+	
     @Override
     public String getName() {
         return null;
@@ -15,6 +29,17 @@ public class AddContactCommand implements Command {
 
     @Override
     public void execute() throws InvalidCommandParameterException {
-
+    	if (parameters.size() != REQUIRED_PARAMETERS_COUNT) {
+    		throw new InvalidCommandParameterException();
+    	}
+    	addContactToRegistry();
     }
+
+	private void addContactToRegistry() {
+		String firstName = parameters.get(0);
+    	String lastName = parameters.get(1);
+    	String email = parameters.get(2);
+    	registry.addContact(firstName, lastName, email);
+	}
+    
 }
--- src/com/loca/addressbook/userinterface/commands/CommandType.java
+++ src/com/loca/addressbook/userinterface/commands/CommandType.java
@@ -1,10 +1,46 @@
 package com.loca.addressbook.userinterface.commands;
 
 public enum CommandType {
-    ADD,
-    LIST,
-    SEARCH,
-    DELETE,
-    HELP,
-    QUIT;
+	
+	ADD(	"add",		3,	"Add a new contact",		"Added the new contact.",	"Add contact failed!"),
+	DELETE(	"delete",	1,	"Delete a contact",			"Deleted the contact.",		"Delete failed! Could not find any matching ID in local address book."),
+	LIST(	"list",		0,	"List all contacts",		"Listed all contacts.",		"List failed! The contact list is empty."),
+	SEARCH(	"search",	1,	"Search for contacts",		"Showed search result.",	"Search failed! Could not find any matching contact."),
+	HELP(	"help",		0,	"Show help information",	"Showed help information.",	"Help request failed!"),
+	QUIT(	"quit",		0,	"Quit program",				"Quitting...",				"Quit request failed");
+	
+	private String commandText;
+	private String description;
+	private int argumentCount;
+	private String successMessage;
+	private String failureMessage;
+	
+	private CommandType(String commandText, int argumentCount, String helpText, String successMessage, String failMessage) {
+		this.commandText = commandText;
+		this.argumentCount = argumentCount;
+		this.description = helpText;
+		this.successMessage = successMessage;
+		this.failureMessage = failMessage;
+	}
+	
+	public String getCommandText() {
+		return commandText;
+	}
+	
+	public int getArgumentCount() {
+		return argumentCount;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getSuccessMessage() {
+		return successMessage;
+	}
+
+	public String getFailureMessage() {
+		return failureMessage;
+	}
+	
 }
