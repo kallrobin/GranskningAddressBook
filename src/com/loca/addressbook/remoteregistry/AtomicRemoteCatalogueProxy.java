db7643273d8e39dca0ce9f1aec650824f00c8da2
Linus Blom <linus.blom@nackademin.se>
2016-12-21 15:46:03 +0100

    Network refactor

--- src/com/loca/addressbook/remoteregistry/AtomicRemoteCatalogueProxy.java
+++ src/com/loca/addressbook/remoteregistry/AtomicRemoteCatalogueProxy.java
@@ -1,5 +1,7 @@
 package com.loca.addressbook.remoteregistry;
 
+import java.io.IOException;
+import java.io.SyncFailedException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,13 +23,30 @@ public class AtomicRemoteCatalogueProxy implements RemoteCatalogueProxy {
 
     @Override
     public List<String> getContacts() {
-        catalogueClient.connect();
-        catalogueClient.sendRequest("getall");
+        try {
+            catalogueClient.connect();
+            catalogueClient.sendRequest("getall");
 
-        String result = catalogueClient.waitForResponse();
-        String[] contactArray = result.split("\n");
-        for (int i = 0; i < contactArray.length; i++) {
-            remoteContactList.add(contactArray[i]);
+            String result = catalogueClient.waitForResponse();
+
+            String[] contactArray = result.split("\n");
+
+            for (int i = 0; i < contactArray.length; i++) {
+                remoteContactList.add(contactArray[i]);
+            }
+
+            catalogueClient.sendRequest("exit");
+
+        } catch (IOException e) {
+            System.err.println("Connection error (" + this.host + ")");
+        } finally {
+            try {
+                catalogueClient.disconnect();
+            } catch (IOException e) {
+                System.err.println("Disconnection error (" + this.host + ")");
+            } catch (NullPointerException e) {
+                System.err.println("Connection never opened (" + this.host + ")");
+            }
         }
         return remoteContactList;
     }
