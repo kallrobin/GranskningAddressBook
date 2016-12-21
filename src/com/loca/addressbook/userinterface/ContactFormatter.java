cd8ff81448db4c71936d91b41a8d6b82305a5005
Linus Blom <linus.blom@nackademin.se>
2016-12-21 12:15:46 +0100

    Small format fix

--- src/com/loca/addressbook/userinterface/ContactFormatter.java
+++ src/com/loca/addressbook/userinterface/ContactFormatter.java
@@ -6,7 +6,7 @@ public class ContactFormatter {
 
     public static String format(Contact contact) {
 
-        return String.format("ID: %s%nFörnamn: %s%nEfternamn: %s%nE-postadress: %s%n%n",
+        return String.format("ID: %s%nFörnamn: %s%nEfternamn: %s%nE-postadress: %s%n",
                 contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getEmailAddress());
     }
 }
