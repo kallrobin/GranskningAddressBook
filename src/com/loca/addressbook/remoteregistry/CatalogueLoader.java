b3f0347e93f7ecc7c9a3bccf30bbbb520473b4be
OlaBKarlsson <ola.karlsson@nackademin.se>
2016-12-21 12:16:26 +0100

    Added more code

--- src/com/loca/addressbook/remoteregistry/CatalogueLoader.java
+++ src/com/loca/addressbook/remoteregistry/CatalogueLoader.java
@@ -1,13 +1,35 @@
 package com.loca.addressbook.remoteregistry;
 
+import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Adrian on 2016-12-20.
  */
 public class CatalogueLoader implements Runnable{
+    int defaultPort = 61616;
+    private RemoteCatalogueFactory catalogueFactory;
+    private RemoteRegistry remoteRegistry;
+    private RemoteCatalogueProxy remoteCatalogueProxy;
+    private List<String> hosts = new ArrayList<>();
 
+    public CatalogueLoader(RemoteRegistry remoteRegistry) {
+        this.remoteRegistry = remoteRegistry;
+    }
 
     @Override
     public void run() {
+        //Add as many hosts as you want
+        hosts.add("localhost");
 
+        for (String host: hosts){
+            new RemoteCatalogueFactory(defaultPort);
+            remoteCatalogueProxy = catalogueFactory.create(host);
+            for(String contact : remoteCatalogueProxy.getContacts()) {
+                String[] splittedList = contact.split(" ");
+                remoteRegistry.add(splittedList[0],splittedList[1],splittedList[2],splittedList[3]);
+            }
+        }
     }
 }
--- src/com/loca/addressbook/remoteregistry/RemoteContact.java
+++ src/com/loca/addressbook/remoteregistry/RemoteContact.java
@@ -2,6 +2,7 @@ package com.loca.addressbook.remoteregistry;
 
 import com.loca.addressbook.registry.Contact;
 import com.loca.addressbook.registry.ContactDetails;
+import com.sun.org.apache.xml.internal.resolver.Catalog;
 
 /**
  * Created by katan on 2016-12-20.
@@ -10,8 +11,10 @@ public class RemoteContact implements Contact {
     private String uuid;
     private ContactDetails contactDetails;
 
+
     public RemoteContact(String firstName, String lastname, String email ) {
         contactDetails = new ContactDetails(firstName,lastname,email);
+
     }
 
     @Override
