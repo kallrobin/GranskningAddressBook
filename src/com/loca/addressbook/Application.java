507ab12ded68090722c94c7c5052b218b8f053b2
christian <christian@ubuntu>
2016-12-21 15:26:53 +0100

    Fully working copy. Well done team LOCA! :-)

--- src/com/loca/addressbook/Application.java
+++ src/com/loca/addressbook/Application.java
@@ -1,5 +1,8 @@
 package com.loca.addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.loca.addressbook.registry.AutoSave;
 import com.loca.addressbook.registry.Registry;
 import com.loca.addressbook.registry.RegistryPersister;
@@ -9,31 +12,58 @@ import com.loca.addressbook.userinterface.CommandLineInterface;
 import com.loca.addressbook.userinterface.Console;
 
 public class Application {
+	
+	private static final String HOSTNAME_1 = "localhost";
+	private static final String HOSTNAME_2 = "172.20.200.173";
+	private static final String HOSTNAME_3 = "172.20.200.240";
     private Console console = new Console();
-    private RegistryPersister registryPersister;
+    private Registry registry = new Registry();
+    private RegistryPersister registryPersister = new RegistryPersister(registry);
+    private RemoteRegistry remoteRegistry = new RemoteRegistry();
 
     public void start() {
-        Registry registry = new Registry();
-        RemoteRegistry remoteRegistry = new RemoteRegistry();
+    	initiateLocalContacts();
+		initiateServerContacts();
+		startAutosaveDaemon();
+		initiateCommandLineInterface();
+    }
 
-        registryPersister = new RegistryPersister(registry);
+	private void initiateLocalContacts() {
         registryPersister.load();
+	}
 
-        Thread autoSave = new Thread(new AutoSave(registryPersister));
+	private void initiateServerContacts() {
+    	List<String> hostNames = makeHostNames();
+		for(String hostName : hostNames) {
+			Runnable runnable = new CatalogueLoader(remoteRegistry, hostName);
+			Thread catalogueLoader = new Thread(runnable);
+	        catalogueLoader.setDaemon(true);
+	        catalogueLoader.start();
+		}
+	}
+	
+	private List<String> makeHostNames() {
+		List<String> hostNames = new ArrayList<>();
+		hostNames.add(HOSTNAME_1);
+		hostNames.add(HOSTNAME_2);
+		// hostNames.add(HOSTNAME_3);
+		return hostNames;
+	}
+
+	private void startAutosaveDaemon() {
+		Runnable runnable  = new AutoSave(registryPersister);
+    	Thread autoSave = new Thread(runnable);
         autoSave.setDaemon(true);
         autoSave.start();
-
-        Thread catalogueLoader = new Thread(new CatalogueLoader(remoteRegistry));
-        catalogueLoader.setDaemon(true);
-        catalogueLoader.start();
-
-        new CommandLineInterface(registry, remoteRegistry, console, this);
-
-
-    }
-
-    public void quit() {
-        registryPersister.save();
-        console.close();
-    }
+	}
+	
+	private void initiateCommandLineInterface() {
+		new CommandLineInterface(registry, remoteRegistry, console, this);
+	}
+	 
+	public void quit() {
+		registryPersister.save();
+		console.close();
+	}
+		
 }
