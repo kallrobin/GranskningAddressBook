7815ad3d94420d264c0fb738325e00655bdf52df
Linus Blom <linus.blom@nackademin.se>
2016-12-21 15:06:14 +0100

    CatalogueLoader update

--- src/com/loca/addressbook/remoteregistry/CatalogueLoader.java
+++ src/com/loca/addressbook/remoteregistry/CatalogueLoader.java
@@ -1,35 +1,24 @@
 package com.loca.addressbook.remoteregistry;
 
-import java.rmi.Remote;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Adrian on 2016-12-20.
- */
 public class CatalogueLoader implements Runnable{
     private static final int PORT = 61616;
     private RemoteCatalogueFactory catalogueFactory = new RemoteCatalogueFactory(PORT);
     private RemoteRegistry remoteRegistry;
-    private RemoteCatalogueProxy remoteCatalogueProxy;
-    private List<String> hosts = new ArrayList<>();
+    private String hostName;
 
-    public CatalogueLoader(RemoteRegistry remoteRegistry) {
+    public CatalogueLoader(RemoteRegistry remoteRegistry, String hostName) {
         this.remoteRegistry = remoteRegistry;
+        this.hostName = hostName;
     }
 
     @Override
     public void run() {
-        //Add as many hosts as you want
-        hosts.add("172.20.200.240");
+        RemoteCatalogueProxy remoteCatalogueProxy = catalogueFactory.create(hostName);
 
-        for (String host: hosts){
-            new RemoteCatalogueFactory(PORT);
-            remoteCatalogueProxy = catalogueFactory.create(host);
-            for(String contact : remoteCatalogueProxy.getContacts()) {
-                String[] splittedList = contact.split(" ");
-                remoteRegistry.add(splittedList[0],splittedList[1],splittedList[2],splittedList[3]);
-            }
+        for(String contact : remoteCatalogueProxy.getContacts()) {
+            String[] splittedList = contact.split(" ");
+            remoteRegistry.add(splittedList[0],splittedList[1],splittedList[2],splittedList[3]);
         }
+
     }
 }
