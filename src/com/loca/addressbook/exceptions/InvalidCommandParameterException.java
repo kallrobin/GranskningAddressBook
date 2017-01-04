e4cf751e8b2849207146d115343af5f52bca3ac7
Linus Blom <linus.blom@nackademin.se>
2017-01-04 11:15:05 +0100

    Removed unused field

--- src/com/loca/addressbook/exceptions/InvalidCommandParameterException.java
+++ src/com/loca/addressbook/exceptions/InvalidCommandParameterException.java
@@ -5,7 +5,6 @@ import com.loca.addressbook.userinterface.commands.CommandType;
 import java.util.List;
 
 public class InvalidCommandParameterException extends Exception {
-    private static final String QUOTATION_MARK = "\"";
 
     public InvalidCommandParameterException (CommandType commandType, List<String> parameters) {
         super("Command Line failed! Received " + parameters.size() + " parameters while expecting " + commandType.getParametersCount() + " parameters.");
